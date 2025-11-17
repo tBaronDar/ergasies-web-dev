a1bf6580c08d33f50173229fd5149a141f097d02
tBaronDar <thedarelis@gmail.com>
2025-11-17 16:25:37 +0200

    added image

--- src/pages/ex-03-start.jsx
+++ src/pages/ex-03-start.jsx
@@ -1,11 +1,26 @@
+import { Link } from 'react-router-dom';
+import meme from '@/assets/acdbq9.jpg';
 function Ex03StartPage() {
-  const paragraphText =
-    'lorem ispum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
   return (
     <div>
       <h1>Θάλασσα - Βραχώδης Ακτή</h1>
-      <p>{paragraphText}</p>
+      <p>
+        <strong>Lorem ispum dolor</strong> sit amet, consectetur adipiscing elit.{' '}
+        <a
+          href='https://www.ionio.gr/index.php?categoryID=149&subcategoryID=218&objectID=6111'
+          target='_blank'
+          rel='noopener noreferrer'>
+          ΕΠΜΣ: Ανάπτυξη Επιχειρησιακών Συστημάτων Λογισμικου
+        </a>{' '}
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Μπορείτε να δείτε περισσότερα στην <Link to='/ex-03b-start'>επόμενη σελίδα.</Link>
+      </p>
+      <img src={meme} alt='Ocean' />
     </div>
   );
 }
